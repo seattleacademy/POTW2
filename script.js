b996bb1730d196625c0dcee348555abf3873ae41
Gary Anderson <ganderson@seattleacademy.org>
2021-03-24 12:30:33 -0700

    Add files for POTW2

---
+++ script.js
@@ -0,0 +1,36 @@
+console.clear();
+const sad = document.getElementById("sad");
+setInterval(upTime, 1000);
+
+function upTime() {
+  let theTime = Number($("#theTime").text());
+  theTime = theTime + 1;
+  $("#theTime").text(theTime);
+}
+
+$("input").change(onChange);
+function enableAll(){
+    $("input").prop( "disabled",false );
+}
+function onChange(evt) {
+  let correct = $(this).data("correct");
+  let response = $(this).val();
+
+  console.log(md5(response));
+  if (correct == md5(response)) {
+    $(this)
+      .removeClass("incorrect")
+      .addClass("correct");
+    let theScore = Number($("#score").text());
+    theScore = theScore + 1;
+    $("#score").text(theScore);
+  } else {
+    sad.play();
+    $(this)
+      .removeClass("correct")
+      .addClass("incorrect");
+      $( this ).prop( "disabled", true );
+
+      setTimeout(enableAll,10000);
+  }
+}
